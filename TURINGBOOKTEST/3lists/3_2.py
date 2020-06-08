b992c2d7361eab26f2e366c2c572a4df476e7f3b
AmosNi <31064508+AmosNi@users.noreply.github.com>
2020-06-08 11:08:40 +0800

    Delete 3_2.py

--- TURINGBOOKTEST/3lists/3_2.py
+++
@@ -1,5 +0,0 @@
-names = ['Amos', 'Blue', 'Yao', 'Chen']
-print(names[0].title() + ', Hello!')
-print(names[1].title() + ', Hello!')
-print(names[2].title() + ', Hello!')
-print(names[3].title() + ', Hello!')
